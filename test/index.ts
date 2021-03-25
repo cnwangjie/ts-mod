f9fb23267cce1ebfdf6aaf9651bf9c4aab178c8c
WangJie <i@i8e.net>
2021-03-25 18:24:14 +0800

    feat: switch test framework to jest

--- test/index.ts
+++
@@ -1,8 +0,0 @@
-import assert = require('assert')
-import { add } from '../src'
-
-describe('add', () => {
-  it('should return the sum of two number', () => {
-    assert.strictEqual(add(1, 2), 3)
-  })
-})
